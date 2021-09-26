42864d59b4db01469f52df3916d952624474bd69
anjanaanil4556 <56733610+anjanaanil4556@users.noreply.github.com>
2021-09-26 15:29:56 +0530

    My_movies.sql file is created
    This file contains my sql queries

---
+++ My_movies.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE My_movies;
+USE My_movies;
+CREATE TABLE Movies(m_name VARCHAR(30),m_actor VARCHAR(20),m_actress VARCHAR(20),m_release YEAR,m_director VARCHAR(20));
+SELECT * FROM Movies;
