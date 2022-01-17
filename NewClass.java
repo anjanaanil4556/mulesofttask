9d4d427bf1fc98a572fe8e11e0f12624b1a06dbf
anjanaanil4556 <56733610+anjanaanil4556@users.noreply.github.com>
2022-01-18 00:15:29 +0530

    Update NewClass.java

--- NewClass.java
+++ NewClass.java
@@ -8,16 +8,16 @@ public class NewClass {
         
         try
         {
-            Class.forName("com.mysql;jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Connecting to the database");
             conn=DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/My_movies","anjana11052001anil8547840894#@");
+                    .getConnection("jdbc:mysql://localhost:3306/My_movies","password");
             System.out.println("You're now connected to the database");
             
             System.out.println("Inserting data");
             stmt=conn.createStatement();
             
-            String sql="INSERT INTO Movies" + 
+            String sql="INSERT INTO Movies " + 
                     "VALUES(\"MULAN\",\"Jet Li\",\"Liu Yifie\",2020,\"Niki Caro\"),(\"CINDERELLA\",\"Richard Madden\",\"Lily James\",2015,\"Kenneth Branagh\"),(\"BEAUTY AND THE BEAST\",\"Dan Stevens\",\"Emma Watson\",2017,\"Bill Condon\"),(\"ALADDIN\",\"Mena Massoud\",\"Naomi Scott\",2019,\"Guy Ritchie\"),(\"SHERLOCK HOLMES\",\"Robert Downey\",\"Rachel McAdams\",2009,\"Guy Ritchie\");";
             stmt.executeUpdate(sql);
             System.out.println("Data inserted");
